491c0a130576a44a53835c2207840081ce04227f
J-Stocks <36339338+J-Stocks@users.noreply.github.com>
2020-10-26 15:34:14 +0000

    Train routes now conform to Laravel conventions with the index on /trains.

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::middleware ('auth') -> group (function () {
-    Route::get('/', [TrainController::class, 'index'])->name('home');
+    Route::get('/trains', [TrainController::class, 'index']);
     Route::get('/trains/{train}', [TrainController::class, 'show']);
 });
