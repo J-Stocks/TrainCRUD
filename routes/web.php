80faff749c3355d68cd43fbc55cc1ce23fbefe74
J-Stocks <36339338+J-Stocks@users.noreply.github.com>
2020-10-26 15:14:11 +0000

    All routes that use the TrainController are now protected by authentication and users are now redirected to the index of trains after login or registration.

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,12 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TrainController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-Route::get('/trains', [TrainController::class, 'index']);
-Route::get('/trains/{train}', [TrainController::class, 'show']);
-
+Route::middleware ('auth') -> group (function () {
+    Route::get('/', [TrainController::class, 'index'])->name('home');
+    Route::get('/trains/{train}', [TrainController::class, 'show']);
+});
